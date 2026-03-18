637fb49d7e730466fa85faa9de1161ceabb06f2f
EmpireWarrior82 <imre.de.haan@gmail.com>
2026-03-18 17:41:59 +0100

    V10.02 Textures and Names added

--- src/main/java/net/empire/ewmedieval/datagen/ModModelProvider.java
+++ src/main/java/net/empire/ewmedieval/datagen/ModModelProvider.java
@@ -751,6 +751,26 @@ public class ModModelProvider extends FabricModelProvider {
         itemModelGenerator.register(ModItems.MUTTON_CHOPS, Models.GENERATED);
 
         itemModelGenerator.register(ModItems.TOMATO_SEEDS, Models.GENERATED);
+        itemModelGenerator.register(ModItems.HAM, Models.GENERATED);
+        itemModelGenerator.register(ModItems.CABBAGE_ROLLS, Models.GENERATED);
+        itemModelGenerator.register(ModItems.SALMON_ROLL, Models.GENERATED);
+        itemModelGenerator.register(ModItems.HAMBURGER, Models.GENERATED);
+        itemModelGenerator.register(ModItems.SALMON_SLICE, Models.GENERATED);
+        itemModelGenerator.register(ModItems.FRUIT_SALAD, Models.GENERATED);
+        itemModelGenerator.register(ModItems.RATATOUILLE, Models.GENERATED);
+        itemModelGenerator.register(ModItems.MIXED_SALAD, Models.GENERATED);
+        itemModelGenerator.register(ModItems.NETHER_SALAD, Models.GENERATED);
+        itemModelGenerator.register(ModItems.STRAW, Models.GENERATED);
+        itemModelGenerator.register(ModItems.BARBECUE_STICK, Models.GENERATED);
+        itemModelGenerator.register(ModItems.COOKED_BACON, Models.GENERATED);
+        itemModelGenerator.register(ModItems.COOKED_MUTTON_CHOPS, Models.GENERATED);
+        itemModelGenerator.register(ModItems.COOKED_CHICKEN_CUTS, Models.GENERATED);
+        itemModelGenerator.register(ModItems.COOKED_COD_SLICE, Models.GENERATED);
+        itemModelGenerator.register(ModItems.COOKED_SALMON_SLICE, Models.GENERATED);
+        itemModelGenerator.register(ModItems.PIE_CRUST, Models.GENERATED);
+        itemModelGenerator.register(ModItems.BEEF_PATTY, Models.GENERATED);
+        itemModelGenerator.register(ModItems.SMOKED_HAM, Models.GENERATED);
+        itemModelGenerator.register(ModItems.COD_SLICE, Models.GENERATED);
 
 
 
